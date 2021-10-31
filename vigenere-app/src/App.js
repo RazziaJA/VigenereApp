c620261d6136b0254152d9ac25158bb756e6079d
RazziaJA <13019628+RazziaJA@users.noreply.github.com>
2021-10-31 18:33:12 -0400

    Added function documentation.

--- vigenere-app/src/App.js
+++ vigenere-app/src/App.js
@@ -1,6 +1,13 @@
 import './App.css';
 import {useState} from "react";
 
+/**
+ * Gets the index of the character in the key to use to shift the plaintext character at the given plaintextIndex. Returns -1 if the key is empty.
+ * 
+ * @param plaintextIndex The index of the character in the plaintext being processed.
+ * @param keyLength The length of the key.
+ * @returns The index of the character to use in the key if the key is non-empty, -1 otherwise.
+ */
 function getKeyIndex(plaintextIndex, keyLength) {
   if (keyLength <= 0) {
     return -1;
@@ -8,14 +15,35 @@ function getKeyIndex(plaintextIndex, keyLength) {
   return plaintextIndex % keyLength;
 }
 
+/**
+ * Checks if the given character is an uppercase or lowercase letter in the English alphabet (A-Z).
+ * 
+ * @param c The character to check.
+ * @returns True if the character is a letter, false otherwise.
+ */
 function isLetter(c) {
   return /[a-zA-Z]/.test(c);
 }
 
+/**
+ * Calculates the given character's place in the alphabet, starting from 'A' = 0. Assumes the character is a letter.
+ * 
+ * @param c The character to index.
+ * @returns The character's place in the alphabet.
+ */
 function getPlaceInAlphabet(c) {
   return c.toLowerCase().charCodeAt(0) - "a".charCodeAt(0);
 }
 
+
+/**
+ * Applies a Vigenere cipher to the character in the given plaintext at the given index, using the given key.
+ * 
+ * @param index The index of the character to shift.
+ * @param plaintext The plaintext.
+ * @param key The key.
+ * @returns The shifted character.
+ */
 function getVigenereCharacterAt(index, plaintext, key) {
   var keyIndex = getKeyIndex(index, key.length);
   if (keyIndex === -1) {
@@ -30,6 +58,13 @@ function getVigenereCharacterAt(index, plaintext, key) {
   return plaintext[index] === plaintext[index].toUpperCase() ? cipherLower.toUpperCase() : cipherLower;
 }
 
+/**
+ * Applies a Vigenere cipher to the given plaintext using the given key.
+ * 
+ * @param plaintext The plaintext.
+ * @param key The key.
+ * @returns The ciphertext.
+ */
 export function vigenere(plaintext, key) {
   var cipher = "";
   for (var i = 0; i < plaintext.length; ++i) {
@@ -38,6 +73,11 @@ export function vigenere(plaintext, key) {
   return cipher;
 }
 
+/**
+ * Creates a React component with two inputs (for plaintext and key) and a label (for ciphertext).
+ * 
+ * @returns The React component.
+ */
 function App() {
   const [plaintext, setPlainText] = useState("");
   const [key, setKey] = useState("");
